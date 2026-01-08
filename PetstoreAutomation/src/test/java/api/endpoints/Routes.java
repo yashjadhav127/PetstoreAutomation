038d96ad7bd87b78aea4bbaeb48bc1bdfb77477d
Yash Jadhav <yashjadhav@2656@gmail.com>
2026-01-08 20:32:52 +0530

    -added all api in routes java

---
+++ PetstoreAutomation/src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,25 @@
+package api.endpoints;
+
+public class Routes {
+	
+	
+	public static String  baseUrl="petstore.swagger.io/v2";
+	 
+	 
+	 //User Model
+	 
+	 public static String post_url= baseUrl+"/user";
+	 
+	 public static String get_url=baseUrl+"/user/{username}";
+	 
+	 public static String update_url=baseUrl+"/user/{username}";
+	 
+	 public static String delete_url=baseUrl+"/user/{username}";
+	 
+	 
+	 
+	
+	
+	
+
+}
